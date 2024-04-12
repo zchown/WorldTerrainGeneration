85ab75281a2136847afcb043a15f36eecaccb0d4
zchown <91749140+zchown@users.noreply.github.com>
2024-04-12 11:28:53 +0200

    added height map

--- src/scene-creation.ts
+++ src/scene-creation.ts
@@ -3,14 +3,16 @@ import * as SHADERS from './shader-module'
 
 export module SceneCreation {
     export function createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
-        var scene = new BABYLON.Scene(engine);
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+        let scene = new BABYLON.Scene(engine);
+        let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
         camera.setTarget(BABYLON.Vector3.Zero());
         camera.attachControl(canvas, true);
-        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+        let light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         light.intensity = 0.7;
 
-        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    let ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6, subdivisions: 500}, scene);
+
+
 
         return scene;
     };
--- src/shader-module.ts
+++ src/shader-module.ts
@@ -1,4 +1,10 @@
+import * as BABYLON from 'babylonjs';
+
 export module ShaderModule {
+    export function hexToVec3(hex: string): BABYLON.Vector3 {
+    return BABYLON.Vector3.FromArray(BABYLON.Color3.FromHexString(hex).toLinearSpace().asArray());
+}
+
 
     
 }
