0b8c9edb685fea041368361f8ffb0f1e151309bd
zchown <91749140+zchown@users.noreply.github.com>
2024-04-12 11:18:34 +0200

    added modules

--- babylon-project/src/AppOne.ts
+++ babylon-project/src/AppOne.ts
@@ -1,19 +1,16 @@
 import * as BABYLON from 'babylonjs'
-export class AppOne {
+import  {SceneCreation} from '../src/scene-creation'
 
-    // Hello!
+export class AppOne {
     engine: BABYLON.Engine;
     scene: BABYLON.Scene;
-
     constructor(readonly canvas: HTMLCanvasElement) {
         this.engine = new BABYLON.Engine(canvas)
         window.addEventListener('resize', () => {
             this.engine.resize();
         });
-        this.scene = createScene(this.engine, this.canvas)
-
+        this.scene = SceneCreation.createScene(this.engine, this.canvas)
     }
-
     debug(debugOn: boolean = true) {
         if (debugOn) {
             this.scene.debugLayer.show({ overlay: true });
@@ -21,26 +18,12 @@ export class AppOne {
             this.scene.debugLayer.hide();
         }
     }
-
     run() {
         this.debug(true);
         this.engine.runRenderLoop(() => {
             this.scene.render();
         });
     }
-
-
-
 }
 
 
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
-    var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, true);
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-
-    return scene;
-};
